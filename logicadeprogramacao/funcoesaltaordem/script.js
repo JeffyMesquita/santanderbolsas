55ca89b97766451946a730af9ced8ec87d523db4
Jeferson Mesquita <je_2742@hotmail.com>
2021-07-04 14:50:47 -0300

    Funções de Alta Ordem
    Funções de Alta Ordem

---
+++ logicadeprogramacao/funcoesaltaordem/script.js
@@ -0,0 +1,5 @@
+const subtrair = (a,b) => a - b ;
+const aplicaOperacao = (a, b, operacao) => operacao(a, b,);
+
+let resultado = aplicaOperacao(4,2, subtrair);
+console.log(resultado);
---
+++ logicadeprogramacao/funcoesaltaordem/script2.js
@@ -0,0 +1,6 @@
+const somarX = (x) => (y) => x + y ;
+
+const somar2 = somarX(2);
+const somar3 = somarX(3);
+
+console.log(somar3(5));
