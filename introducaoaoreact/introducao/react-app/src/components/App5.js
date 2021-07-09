d31f86d93cb5bc07ee3b509e1989f5ec62f8cd03
Jeferson Mesquita <je_2742@hotmail.com>
2021-07-09 18:26:52 -0300

    useEffect
    Componentes Funcionais com Efeitos (Ciclo de Vida)

--- introducaoaoreact/introducao/react-app/src/components/App5.js
+++ introducaoaoreact/introducao/react-app/src/components/App5.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function App5() {
-  const [nome, setNome] = useState('');
+  const [nome, setNome] = useState(undefined);
 
-  const onChange = () => setNome({nome: 'Jeferson'});
-  
+  useEffect(() => {
+    if (nome === undefined) {
+      setNome(sessionStorage.getItem('nome') || '');
+    } else {
+      sessionStorage.setItem('nome', nome);
+    }
+  }, [nome]);
 
   return (
     <>
-      Como devo te chamar?   
-      <input 
-        type="text" 
-        placeholder="Seu nome aqui" 
+      Como devo te chamar?
+      <input
+        type="text"
+        placeholder="Seu nome aqui"
         value={nome}
         onChange={(event) => setNome(event.target.value)}
       />
       <br />
       <label>Olá, {nome}! Tudo bem com você??</label>
     </>
-  )
+  );
 }
