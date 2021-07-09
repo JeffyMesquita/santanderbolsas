c8f370d02f405b22f520ed6960f16448eac98c52
Jeferson Mesquita <je_2742@hotmail.com>
2021-07-09 17:16:28 -0300

    Componente Funcional
    Componente Funcional

---
+++ introducaoaoreact/introducao/react-app/src/components/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Hello = ({text}) => (<h1>{text}</h1>);
+
+
+export default Hello;
--- introducaoaoreact/introducao/react-app/src/index.js
+++ introducaoaoreact/introducao/react-app/src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import App2 from "./components/App2";
-import App4 from "./components/App4";
-import App3 from "./components/App3";
-import {Lista2, Item} from "./components/Lista2";
-import Formulario from "./components/Formulario";
+import Hello from "../src/components/Hello";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Formulario />
+    <Hello text="Hello">
+      <h1>Hello</h1>
+    </Hello>
   </React.StrictMode>,
   document.getElementById("root")
 );
